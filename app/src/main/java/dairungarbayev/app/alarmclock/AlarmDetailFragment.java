eaf463a229eb90c8bf9acd67de1d0aaa12d2cbf1
Dair Ungarbayev <ungarbayevdair@gmail.com>
2019-07-05 00:01:27 +0500

    Alarm detail fragment programmed except for cancel and save buttons. Not tested yet.

--- app/src/main/java/dairungarbayev/app/alarmclock/AlarmDetailFragment.java
+++ app/src/main/java/dairungarbayev/app/alarmclock/AlarmDetailFragment.java
@@ -1,6 +1,11 @@
 package dairungarbayev.app.alarmclock;
 
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,33 +16,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AlarmDetailFragment extends Fragment {
+public class AlarmDetailFragment extends Fragment implements RingtoneChoiceDialogFragment.OnRingtoneChosen {
 
     private Button cancelButton, saveButton, chooseDateButton;
     private TextView overviewTextView;
     private TimePicker timePicker;
     private ToggleButton mondayToggle, tuesdayToggle, wednesdayToggle, thursdayToggle,
                             fridayToggle, saturdayToggle, sundayToggle;
-    private TextView alarmLabel, ringtoneName;
+    private TextView ringtoneName, ringtoneLabel;
     private Switch vibrationSwitch;
     private SeekBar ringtoneVolumeBar;
-    private LinearLayout layoutRingtoneChoice;
-    private CardView cardAlarmLabel;
 
+    private boolean[] weekdays = new boolean[7];
+    private String ringtoneTitle;
+    private Uri ringtoneUri;
+    private int volume;
+    private boolean isVibrationEnabled;
+    private boolean isNew, isRepeating;
+    private int hour, minute;
+    private long date;
+
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     public AlarmDetailFragment() {
-        // Required empty public constructor
     }
 
 
@@ -51,6 +69,17 @@ public class AlarmDetailFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        getReferencesToViews();
+        setTogglesOnListeners();
+        setOnRingtoneChoiceOpener();
+        listenForVibrationSetting();
+        setSeekBar();
+        setTimePicker();
+        setChooseDateButton();
+        setOverViewTextView();
+    }
+
+    private void getReferencesToViews(){
         View view = getView();
         cancelButton = view.findViewById(R.id.button_cancel);
         saveButton = view.findViewById(R.id.button_save);
@@ -67,13 +96,191 @@ public class AlarmDetailFragment extends Fragment {
         saturdayToggle = view.findViewById(R.id.toggle_saturday);
         sundayToggle = view.findViewById(R.id.toggle_sunday);
 
-        cardAlarmLabel = view.findViewById(R.id.card_alarm_label);
-        alarmLabel = view.findViewById(R.id.text_view_alarm_label);
         ringtoneName = view.findViewById(R.id.label_ringtone_name);
-        layoutRingtoneChoice = view.findViewById(R.id.layout_ringtone_choice);
+        ringtoneLabel = view.findViewById(R.id.label_final_ringtone);
         vibrationSwitch = view.findViewById(R.id.switch_vibration);
         ringtoneVolumeBar = view.findViewById(R.id.seek_bar_ringtone_volume);
-        //TODO get references to card views where appropriate
-        //fix ringtone card - the purpose of the app is to wake smn up, not put notifications and reminders
+    }
+
+    private void setTogglesOnListeners(){
+        mondayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[0] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        tuesdayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[1] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        wednesdayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[2] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        thursdayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[3] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        fridayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[4] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        saturdayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[5] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+        sundayToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            weekdays[6] = isChecked;
+            onWeekdayCheckedChanged(isChecked);
+        });
+    }
+
+    private void onWeekdayCheckedChanged(boolean isChecked){
+        if (!isRepeating && isChecked){
+            String msg = getString(R.string.repeat_enabled_message);
+            Toast toast = Toast.makeText(getContext(),msg,Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        isRepeating = isRepeating();
+        setOverViewTextView();
+    }
+
+    private void setOverviewTextRepeating(){
+        StringBuffer buffer = new StringBuffer();
+        String[] weekdaysString = new String[7];
+        weekdaysString[0] = getResources().getString(R.string.monday);
+        weekdaysString[1] = getResources().getString(R.string.tuesday);
+        weekdaysString[2] = getResources().getString(R.string.wednesday);
+        weekdaysString[3] = getResources().getString(R.string.thursday);
+        weekdaysString[4] = getResources().getString(R.string.friday);
+        weekdaysString[5] = getResources().getString(R.string.saturday);
+        weekdaysString[6] = getResources().getString(R.string.sunday);
+
+        for (int i = 0; i<7; i++){
+            if (weekdays[i]){
+                buffer.append(weekdaysString[i]);
+                buffer.append(" ");
+            }
+        }
+
+        overviewTextView.setText(buffer.toString());
+    }
+
+    private void setOverViewTextOneShot(){
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(date);
+        Date date = new Date(cal.getTimeInMillis());
+        SimpleDateFormat format = new SimpleDateFormat("EEE, MMM dd");
+        String dateFormat = format.format(date);
+        overviewTextView.setText(dateFormat);
+    }
+
+    private View.OnClickListener ringtoneChoiceOpener = v -> {
+        RingtoneChoiceDialogFragment dialog = new RingtoneChoiceDialogFragment();
+        dialog.setTargetFragment(AlarmDetailFragment.this, 222);
+        dialog.show(getFragmentManager(), "RingtoneChoiceDialog");
+    };
+
+    private void setOnRingtoneChoiceOpener(){
+        ringtoneName.setOnClickListener(ringtoneChoiceOpener);
+        ringtoneLabel.setOnClickListener(ringtoneChoiceOpener);
+    }
+
+    @Override
+    public void sendRingtoneData(String title, Uri uri) {
+        ringtoneTitle = title;
+        ringtoneUri = uri;
+    }
+
+    private void listenForVibrationSetting(){
+        vibrationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            isVibrationEnabled = isChecked;
+        });
+    }
+
+    private void setSeekBar(){
+        AudioManager manager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        ringtoneVolumeBar.setMax(manager.getStreamMaxVolume(AudioManager.STREAM_ALARM));
+        ringtoneVolumeBar.setProgress(manager.getStreamVolume(AudioManager.STREAM_ALARM));
+
+        ringtoneVolumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                volume = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    private boolean isRepeating(){
+        boolean isRepeating = false;
+        for (int i = 0; i<7; i++){
+            if (weekdays[i]){
+                isRepeating = true;
+            }
+        }
+        return isRepeating;
+    }
+
+    private void setTimePicker(){
+        timePicker.setIs24HourView(true);
+        Calendar cal = Calendar.getInstance();
+        if (isNew){
+            hour = cal.get(Calendar.HOUR_OF_DAY);
+            minute = cal.get(Calendar.MINUTE);
+        }
+        timePicker.setHour(hour);
+        timePicker.setMinute(minute);
+        timePicker.setOnTimeChangedListener((view, newHour, newMinute) -> {
+            hour = newHour;
+            minute = newMinute;
+        });
+    }
+
+    private void setChooseDateButton(){
+        date = Calendar.getInstance().getTimeInMillis();
+        setOverViewTextView();
+
+        dateSetListener = (view, year, month, dayOfMonth) -> {
+            date = getTimeInMillis(year, month, dayOfMonth);
+            if (isRepeating){
+                String msg = getResources().getString(R.string.repeat_disabled_message);
+                Toast toast = Toast.makeText(getContext(),msg,Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            isRepeating = false;
+            setOverViewTextView();
+        };
+        chooseDateButton.setOnClickListener(v -> {
+            DatePickerDialog dialog = new DatePickerDialog(
+                    getContext(),
+                    dateSetListener,
+                    Calendar.getInstance().get(Calendar.YEAR),
+                    Calendar.getInstance().get(Calendar.MONTH),
+                    Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+            );
+            dialog.getDatePicker().setMinDate(Calendar.getInstance().getTimeInMillis());
+            dialog.show();
+        });
+    }
+
+    private void setOverViewTextView(){
+        if (isRepeating){
+            setOverviewTextRepeating();
+        } else {
+            setOverViewTextOneShot();
+        }
+    }
+
+    private long getTimeInMillis(int year, int month, int day){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, 0,0,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        return calendar.getTimeInMillis();
     }
 }
---
+++ app/src/main/java/dairungarbayev/app/alarmclock/RingtoneChoiceDialogFragment.java
@@ -0,0 +1,164 @@
+package dairungarbayev.app.alarmclock;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RingtoneChoiceDialogFragment extends DialogFragment {
+    
+    private static final String TAG = "RingtoneChoiceDialog";
+
+    public interface OnRingtoneChosen{
+        void sendRingtoneData(String title, Uri uri);
+    }
+
+    public OnRingtoneChosen onRingtoneChosen;
+
+    private RadioGroup radioGroup;
+    private Button cancelButton, okButton;
+
+    private ArrayList<String> titles;
+    private ArrayList<Uri> uris;
+    private Uri checkedUri;
+    private String checkedTitle;
+    private boolean previouslyChecked;
+
+    public RingtoneChoiceDialogFragment() {
+        titles = new ArrayList<>();
+        uris = new ArrayList<>();
+        previouslyChecked = false;
+    }
+
+    public RingtoneChoiceDialogFragment(String title, Uri uri){
+        titles = new ArrayList<>();
+        uris = new ArrayList<>();
+        checkedUri = uri;
+        checkedTitle = title;
+        previouslyChecked = true;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            onRingtoneChosen = (OnRingtoneChosen) getTargetFragment();
+        } catch (ClassCastException e){
+            Log.e(TAG, "onAttach: class cast exception " + e.getMessage() );
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.dialog_fragment_ringtone_choice, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getReferencesToViews();
+        getRingtoneData();
+        populateRadioGroup();
+        setCancelButton();
+        setOkButton();
+    }
+
+    private void getReferencesToViews(){
+        View view = getView();
+        radioGroup = view.findViewById(R.id.radio_group_ringtones);
+        cancelButton = view.findViewById(R.id.button_cancel_ringtone_choice);
+        okButton = view.findViewById(R.id.button_ok_ringtone_choice);
+    }
+
+    private void getRingtoneData(){
+        RingtoneManager manager = new RingtoneManager(getContext());
+        manager.setType(RingtoneManager.TYPE_ALARM);
+        Cursor cursor = manager.getCursor();
+        while (cursor.moveToNext() && !cursor.isAfterLast()){
+            Uri uri = manager.getRingtoneUri(cursor.getPosition());
+            uris.add(uri);
+            Ringtone ringtone = manager.getRingtone(cursor.getPosition());
+            titles.add(ringtone.getTitle(getContext()));
+        }
+    }
+
+    private void populateRadioGroup(){
+        int size = titles.size();
+
+        for (int i = 0; i < size; i++){
+            RadioButton radioButton = new RadioButton(getContext());
+            String title = titles.get(i);
+            Uri uri = uris.get(i);
+            radioButton.setText(title);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.topMargin = getResources().getDimensionPixelSize(R.dimen.ringtone_radio_button_margin);
+            radioButton.setLayoutParams(params);
+            radioGroup.addView(radioButton);
+
+            if (previouslyChecked){
+                if (checkedTitle.equals(title) && checkedUri.equals(uri)){
+                    radioButton.toggle();
+                }
+            }
+
+            radioButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    radioButton.toggle();
+
+                    checkedTitle = title;
+                    checkedUri = uri;
+
+                    Ringtone ringtone = RingtoneManager.getRingtone(getContext(),uri);
+                    ringtone.play();
+                }
+            });
+        }
+    }
+
+    private void setCancelButton(){
+        cancelButton.setOnClickListener(v -> {
+            getDialog().dismiss();
+        });
+    }
+
+    private void setOkButton(){
+        okButton.setOnClickListener(v -> {
+            if (radioGroup.getCheckedRadioButtonId() != -1 && !checkedTitle.equals("") && !checkedUri.equals(Uri.EMPTY)){
+                onRingtoneChosen.sendRingtoneData(checkedTitle, checkedUri);
+                getDialog().dismiss();
+            } else {
+                String text = getResources().getString(R.string.please_choose_ringtone);
+                Toast toast = Toast.makeText(getContext(), text, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+}
