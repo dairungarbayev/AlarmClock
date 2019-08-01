2c8c5517fda10fdbaffc56155fc7e1350e5a19af
Dair Ungarbayev <ungarbayevdair@gmail.com>
2019-08-02 00:58:15 +0500

    BootReceiver class declared and receiver added in manifest filtering for locked_boot_completed. Before proceeding, got to get rid of alarms counter key and use highest id to simplify retrieving data.

---
+++ app/src/main/java/dairungarbayev/app/alarmclock/BootReceiver.java
@@ -0,0 +1,15 @@
+package dairungarbayev.app.alarmclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction())) {
+
+        }
+    }
+}
