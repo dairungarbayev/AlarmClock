c69d4813329c23b45bcd71ea4c09de98a705d599
Dair Ungarbayev <ungarbayevdair@gmail.com>
2019-08-04 22:07:42 +0500

    onReceive method implemented and receiver is set enabled in the manifest for now. Not tested yet

--- app/src/main/java/dairungarbayev/app/alarmclock/BootReceiver.java
+++ app/src/main/java/dairungarbayev/app/alarmclock/BootReceiver.java
@@ -3,13 +3,26 @@ package dairungarbayev.app.alarmclock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.ArrayList;
 
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction())) {
-
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+            if (prefs.getAll().size() > 0) {
+                ArrayList<String> jsonList = new ArrayList(prefs.getAll().values());
+                for (String json : jsonList){
+                    CustomAlarm alarm = new CustomAlarm(context.getApplicationContext(),json);
+                    if (alarm.getState()){
+                        alarm.setAlarmOn();
+                    }
+                }
+            }
         }
     }
 }
